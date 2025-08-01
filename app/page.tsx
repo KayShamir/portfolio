6bbcb17eead4b1dc235b24bcadeea7e5bc6e2853
KayShamir <https://github.com/KayShamir>
2025-08-01 18:31:55 +0800

    remove create next app

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./component/hero"
 import FeatureCarousel from "./component/feature-carousel"
 import PortfolioGrid from "./component/portfolio-grid"
 import Timeline from "./component/timeline"
+import Marquee from "./component/marquee"
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <div id="portfolio">
         <PortfolioGrid />
       </div>
+      <Marquee/>
       <div id="journey">
         <Timeline />
       </div>
